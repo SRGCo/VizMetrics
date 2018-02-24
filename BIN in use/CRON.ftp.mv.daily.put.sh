6dad4eb2c0352e9936819520a865d696880b4225
SRG <35151318+SRGCo@users.noreply.github.com>
2018-02-24 11:57:27 -0500

    2-24 changes

--- BIN in use/CRON.ftp.mv.daily.put.sh
+++ BIN in use/CRON.ftp.mv.daily.put.sh
@@ -3,12 +3,18 @@
 set -x
 
 #### LFTP A COPY OF PX FILES TO BERTHA
+## lftp - e (lftp "command")
+## mput - O (specifies base directory)
+## -u (specifies username)
 lftp -e 'set net:timeout 10;set ssl:verify-certificate no; set ftp:ssl-protect-data true;  mput -O / /home/ubuntu/db_files/incoming/px/*; bye' -u px,S3r3n1t33FTP 50.195.41.122 
 
 
 
 #### LFTP A COPY OF PX TO MARKETING VITALS
-lftp -e 'set net:timeout 10;set ssl:verify-certificate no; set ftp:ssl-protect-data true;  mput -O /Paytronix/ /home/ubuntu/db_files/incoming/px/*; bye' -u serenitee-pt,S3tm#XB4z! ftp.marketingvitals.com 
+## lftp - e (lftp "command")
+## mput - O (specifies base directory)
+## -u (specifies username)
+lftp -e 'set net:timeout 10;set ssl:verify-certificate no; set ftp:ssl-protect-data true;  mput -O / /home/ubuntu/db_files/incoming/px/*; bye' -u serenitee-pt,S3tm#XB4z! ftp.marketingvitals.com 
 
 
 
