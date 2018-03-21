541018f23e2b07264847849ad5f8e494f5b549e6
SRG <35151318+SRGCo@users.noreply.github.com>
2018-03-21 13:13:34 -0400

    visit-freq fixes

--- BIN/visitbalance.fix.V1.1card.test.sh
+++ BIN/visitbalance.fix.V1.1card.test.sh
@@ -6,7 +6,7 @@
 
 
 #UNCOMMENT NEXT FOR VERBOSE
-# set -x
+set -x
 ##### HALT AND CATCH FIRE IF ANY COMMANd FAILS
 set -e
 
@@ -16,7 +16,7 @@ set -e
 
 ### what if more than one transaction per day
 
-mysql  --login-path=local -DSRG_Dev -N -e "SELECT DISTINCT(CardNumber) FROM Master_test2 WHERE CardNumber ORDER BY CardNumber ASC" | while read -r CardNumber;
+mysql  --login-path=local -DSRG_Dev -N -e "SELECT DISTINCT(CardNumber) FROM Master_test2 WHERE CardNumber LIKE '6000227902465422'" | while read -r CardNumber;
 do
 	echo $CardNumber
 	######## GET FIRST TRANSACTION
@@ -25,24 +25,30 @@ do
 	######## GET FY FOR THIS TransactionDate (DOB)
 	VisitsAccrued=$(mysql  --login-path=local -DSRG_Dev -N -e "SELECT VisitsAccrued from Master_test2 WHERE TransactionDate = '$Min_dob' and CardNumber = '$CardNumber'")
 
+	echo MinDOB = $Min_dob Visits accrued = $VisitsAccrued
 	##### SELECT ALL ROWS FOR THAT CARD AND SET Vm_VisitsAccrued = VisitsAccrued, Vm_VisitsBalance = VisitsBalance (they are the same)
 	mysql  --login-path=local -DSRG_Dev -N -e "SELECT TransactionDate from Master_test2 WHERE CardNumber = '$CardNumber' ORDER BY TransactionDate ASC" |  while read -r TransactionDate;
 	do
+
+		###### IF THE COUNT OF POSKEYS ON THIS DAY IS MORE THAN ONE ONLY USE THE MAX
+		mysql  --login-path=local -DSRG_Dev -N -e "SELECT MAX(POSkey) from Master_test2 WHERE CardNumber = '$CardNumber' and TransactionDate = '$TransactionDate'" |  while read -r POSkey;
+		do
 			
 			if [ -z "$VisitsAccrued" ]
 			##### IF VisitsAccrued IS NULL (NO EXTRA VISIT ON ENROLLMENT DAY) ##########
 			then
 				##### UPDATE WITH SAME VISIT VALUES
-			mysql  --login-path=local -DSRG_Dev -N -e "UPDATE Master_test2 SET Vm_VisitsAccrued = VisitsAccrued, Vm_VisitsBalance = VisitsBalance WHERE CardNumber = '$CardNumber' and TransactionDate = '$TransactionDate'"
-
+				mysql  --login-path=local -DSRG_Dev -N -e "UPDATE Master_test2 SET Vm_VisitsAccrued = VisitsAccrued, Vm_VisitsBalance = VisitsBalance WHERE POSkey = '$POSkey'"
+	
 			##### IF VisitsAccrued IS NULL (NO EXTRA VISIT ON ENROLLMENT DAY) ##########
 			else
 				##### UPDATE SUBTRACTING 1 FROM ALL VisitsBalance VALUES (to account for visit counted on enrollment day)
-				mysql  --login-path=local -DSRG_Dev -N -e "UPDATE Master_test2 SET Vm_VisitsBalance = (VisitsBalance -1) WHERE CardNumber = '$CardNumber' and TransactionDate = '$TransactionDate'"
+				mysql  --login-path=local -DSRG_Dev -N -e "UPDATE Master_test2 SET Vm_VisitsBalance = (VisitsBalance -1) WHERE POSkey = '$POSkey'"
 
 			fi
-	done
-
+		echo POSkey = $POSkey
+		done
+	do
 	##### UPDATE Vm_VisitsAccrued to ON EARLIEST TRANSACTIONDATE 
 	mysql  --login-path=local -DSRG_Dev -N -e "UPDATE Master_test2 SET Vm_VisitsAccrued = '0' WHERE CardNumber = '$CardNumber' AND TransactionDate = '$Min_dob'"
 
