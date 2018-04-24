1f2e728b4f71b9cf0913e61ea66c83839584e979
SRG <35151318+SRGCo@users.noreply.github.com>
2018-04-24 18:43:40 -0400

    organized files

--- BINDEV/DEV.px.ca.daily.process.V3.1time.sh
+++ BINDEV/DEV.px.ca.daily.process.V3.1time.sh
@@ -15,58 +15,7 @@
 set -e
 
 
-##### BACK IT UP !!!!!!
-# mysqldump  --login-path=local -uroot SRG_Dev > /home/ubuntu/db_files/SRG_Dev_bu.sql
 
-
-
-
-
-############################################################################################
-#### WE WILl MAKE / USE COPIES OF REAL DATA FILES WHILE THIS SCRIPT IS IN DEV #############
-
-# cp /home/ubuntu/db_files/incoming/px/CardActivity*.csv /home/ubuntu/db_files/incoming/px/dev/
-
-#### YEARLY DATA FILES HAVE ONE HEADER ROW (tail -n+2)
-#### DAILY DATA FILES HAVE TWO HEADER ROWS (tail -n+3)
-## REMOVE (1) HEADER ROW AND MERGE (IF NECCESSARY) INCOMING CARD ACTIVITY CSVs
-## INTO SINGLE CARD ACTIVITY FILE IN DB_FILES
-   for file in /home/ubuntu/db_files/incoming/px/CardActivity*.csv
-  do
-      tail -n+2 "$file"  >> /home/ubuntu/db_files/incoming/px/Infile.CardActivity.csv
-  done
- echo 'INCOMING -dev- DATA FILES CLEANED AND MERGED, ARCHIVING ORIGINAL FILES'
-
-
-
-# ARCHIVE THE DOWNLOADED PAYTRONIX FILES
-### copy them for now until archive set up - **** NOT IN DEV ***** -
-mv /home/ubuntu/db_files/incoming/px/CardActivity*.csv /home/ubuntu/db_files/archive/
-### archive trials
-# tar -
-# echo 'ORIGINAL FILES ARCHIVED, DROPPING -OLD- TEMP TABLE'
-
-
-
-# Delete Temp table if it exists
-mysql  --login-path=local --silent -DSRG_Dev -N -e "DROP TABLE IF EXISTS CardActivity_Temp"
-echo 'TEMP TABLE DROPPED, STARTING NEW TEMP TABLE CREATION'
-
-# Create a empty copy of CardActivity table from CardActivityStructure table
-mysql  --login-path=local --silent -DSRG_Dev -N -e "CREATE TABLE CardActivity_Temp LIKE CardActivity_Structure"
-echo 'TEMP TABLE CREATED, LOADING DATA FILE TO TEMP TABLE'
-
-# Load the data from the latest file into the (temp) CardActivity table
-mysql  --login-path=local --silent -DSRG_Dev -N -e "Load data local infile '/home/ubuntu/db_files/incoming/px/Infile.CardActivity.csv' into table CardActivity_Temp fields terminated by ',' lines terminated by '\n'"
-echo 'CARDACTIVITY DATA LOADED INTO CardActivity_Temp'
-
-# DELETE THE WORKING CARDACTIVITY CSV
-#rm -f /home/ubuntu/db_files/incoming/px/CardActivity.csv
-
-### DEV FOR YEARLY FILES
-#mv /home/ubuntu/db_files/incoming/px/*.csv /home/ubuntu/db_files/archive/
-#echo 'CARDACTIVITY -dev- DATA FILES DELETED'
- 
 ### INDEX CARD TEMPLATE AND TRANSACTIONTYPE, CardNumber
 mysql  --login-path=local --silent -DSRG_Dev -N -e "ALTER TABLE CardActivity_Temp ADD INDEX(TransactionType)"
 mysql  --login-path=local --silent -DSRG_Dev -N -e "ALTER TABLE CardActivity_Temp ADD INDEX(CardNumber)"
