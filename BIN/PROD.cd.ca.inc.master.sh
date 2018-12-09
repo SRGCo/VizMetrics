8634e58a9f6921177d06f9dd44f0f4bf492c0e2e
SRG <35151318+SRGCo@users.noreply.github.com>
2018-12-08 19:33:20 -0500

    Exchanges automated daily filehandling

--- BIN/PROD.cd.ca.inc.master.sh
+++ BIN/PROD.cd.ca.inc.master.sh
@@ -181,18 +181,45 @@ trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 echo 'MASTER ACCOUNT STATUSES UPDATED FROM GUESTS MASTER TABLE '
 
 
+
+################# PROCESS EXCHANGES
+## REMOVE (1) HEADER ROW AND MERGE (IF NECCESSARY) INCOMING EXCHANGES CSVs
+## INTO SINGLE CARD ACTIVITY FILE IN DB_FILES
+for file in /home/ubuntu/db_files/incoming/px/MediaExchanges*.csv
+  do
+	#### MAKE A COPY OF THE FILE IN BACKUP DIR
+	cp "$file" //home/ubuntu/db_files/incoming/px/backup/
+	tail -n+2 "$file"  >> /home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv
+	rm "$file"
+  done || trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'INCOMING EXCHANGES DATA FILES BACKEDUP, CLEANED AND MERGED'
+
+## TRUNCATE GUESTS TABLE BEFORE LOADING W NEW
+# Delete Temp table if it exists
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DROP TABLE IF EXISTS Px_exchanges_temp"
+echo 'PX EXCHANGES TEMP TABLE DROPPED, STARTING NEW PX EXCHANGES TEMP TABLE CREATION'
+
+# Create a empty copy of CardActivity table from CardActivityStructure table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "CREATE TABLE Px_exchanges_temp LIKE Px_exchanges_structure"
+echo 'PX EXCHANGES TEMP TABLE CREATED, LOADING DATA FILE TO PX EXCHANGES TEMP TABLE'
+
+# Load the data from the latest file into the (temp) CardActivity table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "Load data local infile '/home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv' into table Px_exchanges_temp fields terminated by ','  lines terminated by '\n'"
+echo 'PX EXCHANGES TEMP loaded'
+	
+#Load the temp data into the live table
+mysql  --login-path=local -DSRG_Prod -N -e "INSERT INTO Px_exchanges SELECT * FROM Px_exchanges_temp"
+echo 'PX EXCHANGES TABLE LOADED WITH DATA FROM TEMP TABLE'
+
+
+# DELETE CURRENT INFILE TO READY FOR NEXT RUN
+rm -f   /home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv
+
+
 ################# PROCESS EXCHANGES WITH PHP SUBROUTINE
 ( "/home/ubuntu/bin/PROD.px.exchanges.process.php" )
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 echo 'MASTER- EXCHANGED CARDS PROCESS/FIXED, ACCOUNT STATUS UPDATED TO -Exchange-'
-
-####### SHOW WHICH HAVE BEEN EXCHANGED IN MASTER ACCOUNT STATUSES
-# mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master JOIN Px_exchanges ON Master.CardNumber = Px_exchanges.CurrentCardNumber SET Master.Account_status = 'Exchange' "
-# trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-# echo 'MASTER- EXCHANGED ACCOUNTS STATUSES UPDATED FROM PX EXCHANGES TABLE'
-
-
-
 
 
 ######### EXCLUDES SECTION USE OR NOT ? ? ?
