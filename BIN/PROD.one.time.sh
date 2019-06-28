fc5214b30f01d3217ed5a7be7801c853b8eb2b62
SRG <35151318+SRGCo@users.noreply.github.com>
2019-06-28 15:22:16 -0400

    Update PROD.one.time.sh
    One time fix for not all data having been pulled across and uber join (only one direction)

--- BIN/PROD.one.time.sh
+++ BIN/PROD.one.time.sh
@@ -26,19 +26,157 @@ failfunction()
 }
 
 
-d=2019-05-01
-while [ "$d" != 2019-05-15 ]; do 
-  echo $d
+
+######### UBER JOIN LIVE CHECK DETAIL WITH LIVE SQUASHED CARD ACTIVITY
+# Delete Temp table if it exists
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DROP TABLE IF EXISTS Master_temp"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TEMP TABLE DROPPED'
+
+# Create a empty copy of CardActivity table from CardActivityStructure table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "CREATE TABLE Master_temp LIKE Master_structure"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TEMP CREATED'
+
+###### WE ONLY GET THE LAST WEEKS WORTH OF DATA
+mysql  --login-path=local -DSRG_Prod -N -e "INSERT INTO Master_temp SELECT CD.*, CA.* FROM CheckDetail_Live AS CD 
+						LEFT JOIN CardActivity_squashed_2 AS CA ON CD.POSkey = CA.POSkey 
+						WHERE CD.DOB >= DATE_SUB(CURDATE(), INTERVAL 6 month) 
+						UNION SELECT CD.*, CA.* FROM .CheckDetail_Live as CD 
+						RIGHT JOIN CardActivity_squashed_2 AS CA ON CD.POSkey = CA.POSkey 
+						WHERE CA.TransactionDate >= DATE_SUB(CURDATE(), INTERVAL 6 month)"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+# echo 'UBER JOIN COMPLETED'
+echo 'MASTER TEMP UPDATED WITH UBER CARD ACTIVITY AND CHECK DETAIL FROM PAST 6 MONTHS'
 
 
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CheckDetail_Live CDT
-	INNER JOIN Employees_Legacy EL ON (CDT.LocationID = EL.LocationID AND CDT.Base_EmployeeID = EL.EmployeeID) 
-	SET CDT.lastname = EL.LastName, CDT.firstname = EL.FirstName
-	WHERE CDT.lastname IS NULL AND CDT.firstname IS NULL"
+# Create enroll_date and Account_status fields
+mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE Master_temp ADD EnrollDate VARCHAR(11)"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-	echo 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# Create ACCOUNT STATUS
+mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE Master_temp ADD Account_status VARCHAR(15)"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-d=$(date -I -d "$d + 1 day")
+# Create ACCOUNT STATUS INDEX
+mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE Master_temp ADD INDEX(Account_status)"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TEMP ENROLLDATE AND ACCOUNT STATUS FIELDS CREATED'
+
+# Create ACCOUNT STATUS
+mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE Master_temp ADD Card_status VARCHAR(15)"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+
+
+# AVOID DUPES DELETE SAME INTERVAL BACK
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM Master WHERE DOB >= DATE_SUB(CURDATE(), INTERVAL 6 month) "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TRUNCATED - USING DOB'
+
+# AVOID DUPES DELETE SAME INTERVAL BACK
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM Master WHERE TransactionDate >= DATE_SUB(CURDATE(), INTERVAL 6 month) "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TRUNCATED - USING TRANSACTIONDATE'
+
+
+
+# COPY THE NEW TRANSACTIONS INTO MASTER
+### WE COULD HAVE THIS TO MAKE SURE THERE ARE RECORDS IN THE TEMP TABLE(?)
+mysql  --login-path=local --silent -DSRG_Prod -N -e "INSERT INTO Master SELECT * FROM Master_temp"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER POPULATED FROM MASTER TEMP'
+
+
+
+############## THE NEXT SECTIONS WILL GET MOVED AROUND IF WE ADD CARD STATUS FIELDS
+####### MASTER TABLE GUEST INFO UPDATE
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master JOIN Guests_Master ON Master.CardNumber = Guests_Master.CardNumber 
+							SET Master.EnrollDate = Guests_Master.EnrollDate, Master.Account_status = Guests_Master.AccountStatus"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER ACCOUNT STATUSES UPDATED FROM GUESTS MASTER TABLE '
+
+
+
+################# PROCESS EXCHANGES WITH PHP SUBROUTINE
+( "/home/ubuntu/bin/PROD.px.exchanges.process.php" )
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER- EXCHANGED CARDS PROCESS/FIXED, ACCOUNT STATUS UPDATED TO -Exchange-'
+
+
+######### EXCLUDES SECTION USE OR NOT ? ? ?
+# mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master JOIN Excludes ON Master.CardNumber = Excludes.CardNumber SET Master.Account_status = 'Exclude' "
+# trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER - NO ACCOUNTS EXCLUDED!!! (Exclusion routine commented out)'
+
+
+
+
+
+
+
+
+######## UPDATE THE EMPTY CHECKDETAIL FIELDS WITH PX DATA
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET CheckNumber = CheckNo_px WHERE CheckNumber IS NULL "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER EMPTY CHECKNO POPULATED FROM PX DATA'
+
+
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET LocationID = LocationID_px WHERE LocationID IS NULL "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER EMPTY LOCATION ID POPULATED FROM PX DATA'
+
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET POSkey = POSKey_px WHERE POSkey IS NULL "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER EMPTY POS KEYS POPULATED FROM PX DATA'
+
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET DOB = TransactionDate WHERE DOB IS NULL "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER EMPTY DOB POPULATED FROM PX DATA'
+
+
+
+mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET GrossSalesCoDefined = DollarsSpentAccrued WHERE GrossSalesCoDefined IS NULL 
+						AND Master.Account_status <> 'TERMIN' AND Master.Account_status <> 'SUSPEN' 
+						AND Master.Account_status <> 'Exchanged' AND Master.Account_status <> 'Exchange' 
+						AND Master.Account_status <> 'Exclude' AND DollarsSpentAccrued IS NOT NULL"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER GROSSSALESCODEFINED FIELD POPULATED'
+echo '(PROMOS OR COMPS COULD NOT BE ADDED, LOWBALL FIGURES)'
+
+
+###### -N is the No Headers in Output option
+###### -e is the 'read statement and quit'
+######## WE ARE ###
+
+mysql  --login-path=local -DSRG_Prod -N -e "SELECT Master.DOB FROM Master WHERE Master.DOB IS NOT NULL AND DOB >= DATE_SUB(NOW(), INTERVAL 6 month) 
+				GROUP BY Master.DOB ORDER BY Master.DOB DESC" | while read -r DOB;
+do
+
+		######## GET FY FOR THIS DOB (DOB)
+		FY=$(mysql  --login-path=local -DSRG_Prod -N -e "SELECT FY from Lunas WHERE DOB = '$DOB'")
+
+		######## GET FY FOR THIS DOB (DOB)
+		YLuna=$(mysql  --login-path=local -DSRG_Prod -N -e "SELECT YLuna from Lunas WHERE DOB = '$DOB'")
+
+		######## GET FY FOR THIS DOB (DOB)
+		Luna=$(mysql  --login-path=local -DSRG_Prod -N -e "SELECT Luna from Lunas WHERE DOB = '$DOB'")
+
+		######## IF VARIABLE HAS NO VALUE SET TO NULL
+		if [ -z $Luna ] 
+		then 
+		Luna='0'
+		fi
+
+		##### UPDATE FISCAL YEAR FROM DOB
+		mysql  --login-path=local -DSRG_Prod -N -e "UPDATE Master SET FY = '$FY',YLuna = '$YLuna', Luna='$Luna' WHERE Master.DOB = '$DOB'"
+		#echo $DOB updated FY= $FY YLuna = $YLuna  Luna = $Luna
+
 done
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER FY YLUNA FIELDS UPATED WITH DATA FROM LUNA TABLE'
+
+
+
+
+
 
