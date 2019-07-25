e2ea95f1c22033f099d153a262c1060663264c94
SRG <35151318+SRGCo@users.noreply.github.com>
2019-07-25 13:32:38 -0400

    Create PROD.one.time.sh
    rebuild the px_exchanges table

---
+++ BIN/PROD.one.time.sh
@@ -0,0 +1,63 @@
+#! //bin/bash
+# LOG IT TO SYSLOG
+# exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+# THIS SCRIPT HAS TO RUN AFTER CHECKDETAIL IS PROCESSED SO THAT THE CHECK NUMBER FIX RUNS CORRECTLY
+
+# UNCOMMENT NEXT FOR VERBOSE
+# set -x
+
+
+
+################# ERROR CATCHING ##########################
+failfunction()
+{
+	local scriptname=$(basename -- "$0") 
+	local returned_value=$1
+	local lineno=$2
+	local bash_error=$3
+
+	if [ "$returned_value" != 0 ]
+	then 
+ 		echo "$scriptname failed on $bash_error at line: $lineno"
+        	mail -s "VizMetrics Server Alert"  it@serenitee.com <<< 'Script '"$scriptname"' failed on '"$bash_error"' at Line: '"$lineno"
+        	exit
+	fi
+}
+
+################# PROCESS EXCHANGES
+## REMOVE (1) HEADER ROW AND MERGE (IF NECCESSARY) INCOMING EXCHANGES CSVs
+## INTO SINGLE CARD ACTIVITY FILE IN DB_FILES
+for file in /home/ubuntu/db_files/incoming/px/MediaExchanges*.csv
+  do
+	#### MAKE A COPY OF THE FILE IN BACKUP DIR
+	cp "$file" //home/ubuntu/db_files/incoming/px/backup/
+	tail -n+2 "$file"  >> /home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv
+	rm "$file"
+  done || trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'INCOMING EXCHANGES DATA FILES BACKEDUP, CLEANED AND MERGED'
+
+## TRUNCATE GUESTS TABLE BEFORE LOADING W NEW
+# Delete Temp table if it exists
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DROP TABLE IF EXISTS Px_exchanges_temp"
+echo 'PX EXCHANGES TEMP TABLE DROPPED, STARTING NEW PX EXCHANGES TEMP TABLE CREATION'
+
+# Create a empty copy of CardActivity table from CardActivityStructure table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "CREATE TABLE Px_exchanges_temp LIKE Px_exchanges_structure"
+echo 'PX EXCHANGES TEMP TABLE CREATED, LOADING DATA FILE TO PX EXCHANGES TEMP TABLE'
+
+# Load the data from the latest file into the (temp) CardActivity table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "Load data local infile '/home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv' into table Px_exchanges_temp fields terminated by ','  lines terminated by '\n'"
+echo 'PX EXCHANGES TEMP loaded'
+	
+#Load the temp data into the live table
+mysql  --login-path=local -DSRG_Prod -N -e "INSERT INTO Px_exchanges SELECT * FROM Px_exchanges_temp"
+echo 'PX EXCHANGES TABLE LOADED WITH DATA FROM TEMP TABLE'
+
+
+# DELETE CURRENT INFILE TO READY FOR NEXT RUN
+rm -f   /home/ubuntu/db_files/incoming/px/Infile.MediaExchanges.csv
+
+
+
+
