173119e0ef932fd5b716c366a824e72eb16e5b7e
SRG <35151318+SRGCo@users.noreply.github.com>
2019-12-13 14:42:40 -0500

    Update DEV.one.time.sh

--- BIN/DEV.one.time.sh
+++ BIN/DEV.one.time.sh
@@ -24,16 +24,30 @@ failfunction()
 	fi
 }
 
-########### UPDATE THE CardActivitylive table
-mysql  --login-path=local --silent -DSRG_Dev -N -e "INSERT INTO CardActivity_Live SELECT * FROM CardActivity_Temp"
+
+## REMOVE (1) HEADER ROW AND MERGE (IF NECCESSARY) INCOMING CARD ACTIVITY CSVs
+## INTO SINGLE CARD ACTIVITY FILE IN DB_FILES
+for file in /home/ubuntu/db_files/incoming/px/CardActivity*.csv
+  do
+	#### MAKE A COPY OF THE FILE IN BACKUP DIR
+	cp "$file" //home/ubuntu/db_files/incoming/px/backup/
+	tail -n+2 "$file"  >> /home/ubuntu/db_files/incoming/px/Infile.CardActivity.csv
+  done || trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'INCOMING PX {dev} DATA FILES CLEANED AND MERGED, ARCHIVING ORIGINAL FILES'
+
+
+# Delete Temp table if it exists
+mysql  --login-path=local --silent -DSRG_Prod -N -e "DROP TABLE IF EXISTS CardActivity_Temp"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'CARD ACTIVITY LIVE TABLE UPDATED WITH CARD ACTIVITY TEMP DATA'
+echo 'CardActivity_Temp TABLE DROPPED, STARTING NEW TEMP TABLE CREATION'
 
-echo 'DEV.PX.CA.PROCESS.SH COMPLETED'
-
-
-###### PROCESS THE CA_Live TABLE INTO FIRST USE TABLE
-( "/home/ubuntu/bin/DEV.app.use.firstserver.php" )
+# Create a empty copy of CardActivity table from CardActivityStructure table
+mysql  --login-path=local --silent -DSRG_Prod -N -e "CREATE TABLE CardActivity_Temp LIKE CardActivity_Structure"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'DEV App_use1 Table populated with new data'
+
+# Load the data from the latest file into the (temp) CardActivity table
+############### DUMMY FIELDS TO KEEP ALIGNMENT WITH PAYTRONIX ALTERATIONS TO TABLE STRUCTURE
+mysql  --login-path=local --silent -DSRG_Prod -N -e "Load data local infile '/home/ubuntu/db_files/incoming/px/Infile.CardActivity.csv' into table CardActivity_Raw fields terminated by ',' lines terminated by '\n' (CardNumber,AccountCode,CustomerNo,CardTemplate,TransactionDate,TransactionType,StoreMerchant,StoreNumber,StoreName,WalletType,CheckNo,TerminalID,CashierID,IdentificationMethod,AccountStatus,Promotion,AuthCode,Sender,Dummy1,Dummy2,Dummy3,Dummy4,Dummy5,Dummy6,Dummy_Checkins_Accrued,Dummy_Checkins_Redeemed,Dummy_Checkins_Balance,Checkins_Accrued,Checkins_Redeemed,Checkins_Balance,Qualified_Checkins_Accrued,Qualified_Checkins_Redeemed,Qualified_Checkins_Balance,SurveyAccrued,SurveyRedeemed,SurveyBalance,NewsletterAccrued,NewsletterRedeemed,NewsletterBalance,LifetimeSpendAccrued,LifetimeSpendRedeemed,LifetimeSpendBalance,3000BonusPointsAccrued,3000BonusPointsRedeemed,3000BonusPointsBalance,RegAppAccrued,RegAppRedeemed,RegAppBalance,BdayEntreeAccrued,BdayEntreeRedeemed,BdayEntreeBalance,Dummy7,Dummy8,Dummy9,LTOAccrued,LTORedeemed,LTOBalance,LTObucksAccrued,LTObucksRedeemed,LTObucksBalance,CheckSubtotalAccrued,CheckSubtotalRedeemed,CheckSubtotalBalance,DollarsSpentAccrued,DollarsSpentRedeemed,DollarsSpentBalance,KidsMenuTrackingAccrued,KidsMenuTrackingRedeemed,KidsMenuTrackingBalance,BeerTrackingAccrued,BeerTrackingRedeemed,BeerTrackingBalance,SushiTrackingAccrued,SushiTrackingRedeemed,SushiTrackingBalance,WineTrackingAccrued,WineTrackingRedeemed,WineTrackingBalance,StoreRegisteredAccrued,StoreRegisteredRedeemed,StoreRegisteredBalance,SereniteePointsAccrued,SereniteePointsRedeemed,SereniteePointsBalance,LifetimePointsAccrued,LifetimePointsRedeemed,LifetimePointsBalance,100PointsIncrementAccrued,100PointsIncrementRedeemed,100PointsIncrementBalance,FreeAppAccrued,FreeAppRedeemed,FreeAppBalance,Dummy10,Dummy11,Dummy12,FreeEntreeAccrued,FreeEntreeRedeemed,FreeEntreeBalance,FreeDessertAccrued,FreeDessertRedeemed,FreeDessertBalance,FreePizzaAccrued,FreePizzaRedeemed,FreePizzaBalance,FreeSushiAccrued,FreeSushiRedeemed,FreeSushiBalance,5500PointsAccrued,5500PointsRedeemed,5500PointsBalance,3500PointsAccrued,3500PointsRedeemed,3500PointsBalance,2500PointsAccrued,2500PointsRedeemed,2500PointsBalance,1Kpts5bksAccrued,1Kpts5bksRedeemed,1Kpts5bksBalance,VisitsAccrued,VisitsRedeemed,VisitsBalance,TWKTripAccrued,TWKTripRedeemed,TWKTripBalance,SpotTripAccrued,SpotTripRedeemed,SpotTripBalance,MagsTripAccrued,MagsTripRedeemed,MagsTripBalance,OpusTripAccrued,OpusTripRedeemed,OpusTripBalance,WalnutTripAccrued,WalnutTripRedeemed,WalnutTripBalance,HaleTripAccrued,HaleTripRedeemed,HaleTripBalance,CalasTripAccrued,CalasTripRedeemed,CalasTripBalance,LatTripAccrued,LatTripRedeemed,LatTripBalance,HBTripAccrued,HBTripRedeemed,HBTripBalance,SereniteeAccrued,SereniteeRedeemed,SereniteeBalance,BandCompAccrued,BandCompRedeemed,BandCompBalance,GreenDollarsAccrued,GreenDollarsRedeemed,GreenDollarsBalance,GreenLATAppAccrued,GreenLATAppRedeemed,GreenLATAppBalance,GreenALCAppAccrued,GreenALCAppRedeemed,GreenALCAppBalance,GreenOPUSAppAccrued,GreenOPUSAppRedeemed,GreenOPUSAppBalance,GreenCALAppAccrued,GreenCALAppRedeemed,GreenCALAppBalance,GreenSPOTAppAccrued,GreenSPOTAppRedeemed,GreenSPOTAppBalance,GreenHALEAppAccrued,GreenHALEAppRedeemed,GreenHALEAppBalance,GreenWINCAppAccrued,GreenWINCAppRedeemed,GreenWINCAppBalance,GreenMAGsAppAccrued,GreenMAGsAppRedeemed,GreenMAGsAppBalance,GreenWALAppAccrued,GreenWALAppRedeemed,GreenWALAppBalance,CompAccrued,CompRedeemed,CompBalance,SereniteeGiftCardAccrued,SereniteeGiftCardRedeemed,SereniteeGiftCardBalance,SVDiscountTrackingAccrued,SVDiscountTrackingRedeemed,SVDiscountTrackingBalance)"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'CARDACTIVITY DATA LOADED INTO CardActivity_Temp'
 
