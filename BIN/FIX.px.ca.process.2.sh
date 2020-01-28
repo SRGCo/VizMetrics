ada12478a9e9a4bf9cca2b8167e5f3f0be4a9566
SRG <35151318+SRGCo@users.noreply.github.com>
2020-01-28 11:18:38 -0500

    Fix part 2 updated

--- BIN/FIX.px.ca.process.2.sh
+++ BIN/FIX.px.ca.process.2.sh
@@ -2,10 +2,20 @@
 # LOG IT TO SYSLOG
 # exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-# THIS SCRIPT HAS TO RUN AFTER CHECKDETAIL IS PROCESSED SO THAT THE CHECK NUMBER FIX RUNS CORRECTLY
-
 # UNCOMMENT NEXT FOR VERBOSE
-#set -x
+# set -x
+##### HALT AND CATCH FIRE IF ANY COMMAND FAILS
+# set -e
+
+##### USE time command to calc runtime "time DEV.cd.ca.into.master.sh"
+
+
+############# THIS SCRIPT USES THE FOLLOWING
+# PROD.wrong.enroll.fix.php
+# PROD.VM_visits.master.process.sh
+############# above uses: PROD.visitbalance.fix.php
+
+
 
 
 ################# ERROR CATCHING ##########################
@@ -25,152 +35,222 @@ failfunction()
 }
 
 
-#### THIS IS ONLY FOR THE CA DATA FROM 2019-07-11 THROUGH 2019-10-09
-
-sleep 5s
-
-##### HALT AND CATCH FIRE IF ANY COMMAND FAILS FROM HERE ON
-set -e
+##### UPDATE FISCAL YEAR FROM DOB
+	mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "DELETE FROM CardActivity_squashed_2 WHERE LocationID = '0'"
+	echo 'LOCATION 0 TRANSACTIONS DUMPED FROM CA_S_2'
 
 
 
-### INDEX CARD TEMPLATE AND TRANSACTIONTYPE, CardNumber
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD INDEX(TransactionType)"
+
+######### UBER JOIN LIVE CHECK DETAIL WITH LIVE SQUASHED CARD ACTIVITY
+# Delete Temp table if it exists
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "DROP TABLE IF EXISTS Master_temp"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TEMP TABLE DROPPED'
+
+# Create a empty copy of CardActivity table from CardActivityStructure table
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "CREATE TABLE Master_temp LIKE Master_structure"
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TEMP CREATED'
+
+###### WE ONLY GET THE LAST WEEKS WORTH OF DATA
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "INSERT INTO Master_temp SELECT CD.*, CA.* FROM CheckDetail_Live AS CD 
+						LEFT JOIN CardActivity_squashed_2 AS CA ON CD.POSkey = CA.POSkey  
+						UNION SELECT CD.*, CA.* FROM .CheckDetail_Live as CD 
+						RIGHT JOIN CardActivity_squashed_2 AS CA ON CD.POSkey = CA.POSkey "
+trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+# echo 'UBER JOIN COMPLETED'
+echo 'MASTER TEMP POPULATED WITH UBER CARD ACTIVITY AND CHECK DETAIL'
+
+# Create enroll_date and Account_status fields
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "ALTER TABLE Master_temp ADD EnrollDate VARCHAR(11)"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD INDEX(CardNumber)"
+# Create ACCOUNT STATUS
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "ALTER TABLE Master_temp ADD Account_status VARCHAR(15)"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-echo 'CARDACTIVITY -dev- TransactionType and CardNumber indexed'
-
-
-echo 'DELETING EXTRANEOUS RECORDS BY TRANSACTION TYPES'
-### REMOVE ANY/ALL RECORDS THAT ARE NOT WORTH PROCESSING ! ! ! !
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE CardTemplate != 'Serenitee Loyalty'"
-echo '10% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Identify Customer'"
-echo '25% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Web Reward Purchase'"
-echo '30% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Admin Adjustment'"
-echo '35% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Denied Campaign Adjustment'"
-echo '40% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Denied Accrual / Redemption'"
-echo '45% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Denied Activate'"
-echo '50% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Check-In'"
-echo '55% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Campaign Adjustment'"
-echo '60% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Balance Inquiry'"
-echo '65% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Denied Balance Inquiry'"
-echo '70% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType = 'Campaign Expiration'"
-echo '75% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE TransactionType IS NULL"
-echo '90% deleted'
-mysql  --login-path=local --silent -DSRG_Prod -N -e "DELETE FROM CardActivity_Temp WHERE CardNumber = '0'"
-echo '100% deleted, ADDING LOCATIONID FIELD'
-
-# CREATE LOCATIONID FIELD
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD LocationID INT( 3 ) first"
+# Create ACCOUNT STATUS INDEX
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "ALTER TABLE Master_temp ADD INDEX(Account_status)"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'ADDED LOCATIONID FIELD TO TEMP TABLE, UPDATING LOCATIONS'
+echo 'MASTER TEMP ENROLLDATE AND ACCOUNT STATUS FIELDS CREATED'
 
-##### UPDATE LOCATIONID
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp set LocationID = (SELECT ID from Locations WHERE Locations.PXID = CardActivity_Temp.StoreNumber)"
-trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'UPDATED LOCATIONID FROM locations TABLE, FORMATTING TransactionDate FIELD'
-
-##### UPDATE RAW DOB TO VARCHAR
-# UPDATE THE DOB TO VARCHAR
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp modify TransactionDate VARCHAR(40)"
+# Create ACCOUNT STATUS
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "ALTER TABLE Master_temp ADD Card_status VARCHAR(15)"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD COLUMN TransactionTime VARCHAR(10) AFTER TransactionDate"
+
+# COPY THE NEW TRANSACTIONS INTO MASTER
+### WE COULD HAVE THIS TO MAKE SURE THERE ARE RECORDS IN THE TEMP TABLE(?)
+mysql  --login-path=local --silent -DSRG_Prod-1-27-20 -N -e "INSERT INTO Master SELECT * FROM Master_temp"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER POPULATED FROM MASTER TEMP'
 
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp SET TransactionTime = RIGHT(TransactionDate, 5)"
+
+
+############## THE NEXT SECTIONS WILL GET MOVED AROUND IF WE ADD CARD STATUS FIELDS
+####### MASTER TABLE GUEST INFO UPDATE
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master JOIN Guests_Master ON Master.CardNumber = Guests_Master.CardNumber 
+							SET Master.EnrollDate = Guests_Master.EnrollDate, Master.Account_status = Guests_Master.AccountStatus"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER ACCOUNT STATUSES UPDATED FROM GUESTS MASTER TABLE '
 
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp SET TransactionDate = LEFT(TransactionDate,10)"
+
+
+################# PROCESS EXCHANGES WITH PHP SUBROUTINE
+( "/home/ubuntu/bin/FIX.px.exchanges.process.php" )
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER- EXCHANGED CARDS PROCESS/FIXED, ACCOUNT STATUS UPDATED TO -Exchange-'
 
-echo 'DOB NOW VARCHAR, UPDATING TO SQL'
 
-# PUT TransactionDate INTO SQL FORMAT
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp SET TransactionDate= STR_TO_DATE(TransactionDate, '%Y-%m-%d') WHERE STR_TO_DATE(TransactionDate, '%Y-%m-%d') IS NOT NULL"
+######### EXCLUDES SECTION USE OR NOT ? ? ?
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master JOIN Excludes ON Master.CardNumber = Excludes.CardNumber SET Master.Account_status = 'Exclude' "
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'DOB NOW SQL FORMAT, UPDATING TO DATE FORMAT'
+echo 'MASTER - Only SRG Office + Zorba Marked Exclude (using Excludes table), other employees etc STILL BEING PROCESSED'
 
-# Change TransactionDate field to type date
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp CHANGE TransactionDate TransactionDate DATE"
+
+
+
+
+
+
+
+######## UPDATE THE EMPTY CHECKDETAIL FIELDS WITH PX DATA
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET CheckNumber = CheckNo_px WHERE CheckNumber IS NULL "
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'DOB NOW DATE FORMAT, ADDING POSkey field'
+echo 'MASTER EMPTY CHECKNO POPULATED FROM PX DATA'
 
-# Create POSkey field
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD POSkey VARCHAR(30) first"
+
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET LocationID = LocationID_px WHERE LocationID IS NULL "
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'POSkey FIELD ADDED, ADDING Exceldate FIELD'
+echo 'MASTER EMPTY LOCATION ID POPULATED FROM PX DATA'
 
-# Create excel date field
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD Exceldate INT(100) AFTER LocationID"
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET POSkey = POSKey_px WHERE POSkey IS NULL "
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'Exceldate FIELD ADDED, POPULATING ExcelDate FIELD'
+echo 'MASTER EMPTY POS KEYS POPULATED FROM PX DATA'
 
-# Update excel date field
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp set Exceldate = (((unix_timestamp(TransactionDate) / 86400) + 25569) + (-5/24))"
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET DOB = TransactionDate WHERE DOB IS NULL "
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'ExcelDate FIELD POPULATED, CREATING POSkey VALUES'
+echo 'MASTER EMPTY DOB POPULATED FROM PX DATA'
 
 
-# Update POSkey field (location + TransactionDate[excel format][no decimal] + checknumber)
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp set POSkey = CONCAT_WS('', LocationID, Exceldate, CheckNo)"
+
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET GrossSalesCoDefined = DollarsSpentAccrued WHERE GrossSalesCoDefined IS NULL 
+						AND Master.Account_status <> 'TERMIN' AND Master.Account_status <> 'SUSPEN' 
+						AND Master.Account_status <> 'Exchanged' AND Master.Account_status <> 'Exchange' 
+						AND Master.Account_status <> 'Exclude' AND DollarsSpentAccrued IS NOT NULL"
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'POSkeys CREATED'
+echo 'MASTER GROSSSALESCODEFINED FIELD POPULATED'
+echo '(PROMOS OR COMPS COULD NOT BE ADDED, LOWBALL FIGURES)'
 
 
-### INDEX CARD TEMPLATE AND TRANSACTIONTYPE, CardNumber
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp ADD INDEX(CheckNo)"
-trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'CARDACTIVITY -dev- CheckNo indexed'
+###### -N is the No Headers in Output option
+###### -e is the 'read statement and quit'
+######## WE ARE ###
 
-
-#### !!!!!!!! 	WE COULD HAVE THE SELECT QUERY ONLY GO BACK x# OF DAYS   !!!!!! ####
-############ ************** CAN WE SPEED THIS UP ******************** ##############
-##################### ITERATE UPDATE TO CA CheckNumbers MISSING LEADIN "100"
-############################## THIS IS WHY CHECKDETAIL HAS TO RUN EARLIER THAN CA 
-
-mysql  --login-path=local --silent -DSRG_Prod -N -e "SELECT RIGHT(CheckNumber, 4), DOB, LocationID FROM CheckDetail_Live WHERE CheckDetail_Live.CheckNumber like '100%' AND DOB >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) ORDER BY DOB ASC" | while read -r CheckNumber DOB LocationID;
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT Master.DOB FROM Master WHERE Master.DOB IS NOT NULL
+				GROUP BY Master.DOB ORDER BY Master.DOB DESC" | while read -r DOB;
 do
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp SET CheckNo=CONCAT('100',CheckNo) WHERE CheckNo = '$CheckNumber' AND TransactionDate = '$DOB' AND LocationID = '$LocationID' AND char_length(CheckNo) < '6'"
+
+		######## GET FY FOR THIS DOB (DOB)
+		FY=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT FY from Lunas WHERE DOB = '$DOB'")
+
+		######## GET FY FOR THIS DOB (DOB)
+		YLuna=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT YLuna from Lunas WHERE DOB = '$DOB'")
+
+		######## GET FY FOR THIS DOB (DOB)
+		Luna=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT Luna from Lunas WHERE DOB = '$DOB'")
+
+		######## IF VARIABLE HAS NO VALUE SET TO NULL
+		if [ -z $Luna ] 
+		then 
+		Luna='0'
+		fi
+
+		##### UPDATE FISCAL YEAR FROM DOB
+		mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET FY = '$FY',YLuna = '$YLuna', Luna='$Luna' WHERE Master.DOB = '$DOB'"
+		#echo $DOB updated FY= $FY YLuna = $YLuna  Luna = $Luna
+
 done
 trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'PX CHECKNUMBERS MISSING 100 FIXED, UPDATING POSKEYS IN TEMP TABLE'
-
-echo 'CORRELATING/FIXING PX CHECKNUMBERS MISSING 100'
-##### Update POSkey field (location + TransactionDate[excel format][no decimal] + checknumber)
-mysql  --login-path=local --silent -DSRG_Prod -N -e "UPDATE CardActivity_Temp set POSkey = CONCAT_WS('', LocationID, Exceldate, CheckNo)"
-trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'UPDATED POSKEYS IN TEMP TABLE'
-
-######## DROP UNNEEDED TEMP FIELDS
-mysql  --login-path=local --silent -DSRG_Prod -N -e "ALTER TABLE CardActivity_Temp DROP Exceldate"
-trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'DROPPED EXCEL DATE FIELD FROM CARDACTIVITY TEMP'
+echo 'MASTER FY YLUNA FIELDS UPATED WITH DATA FROM LUNA TABLE'
 
 
-########### UPDATE THE CardActivitylive table
-mysql  --login-path=local --silent -DSRG_Prod -N -e "INSERT INTO CardActivity_Live SELECT * FROM CardActivity_Temp"
-trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
-echo 'CARD ACTIVITY LIVE TABLE UPDATED WITH CARD ACTIVITY TEMP DATA'
+mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT DISTINCT(CardNumber) FROM Master WHERE CardNumber IS NOT NULL ORDER BY CardNumber ASC" | while read -r CardNumber;
+do
 
+	######## COUNT VISITS OVER PREVIOUS 12 MONTHS AND LIFETIME
+	PrevYear=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT COUNT(*) from Master WHERE CardNumber = '$CardNumber' AND TransactionDate <> EnrollDate 
+								AND Vm_VisitsAccrued = '1' AND TransactionDate >= DATE_SUB(NOW(),INTERVAL 365 DAY)")
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+	######## MINIMUM VISITBALNCE
+	MinBal=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT MIN(Vm_Visitsbalance) from Master WHERE CardNumber = '$CardNumber'")
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
+	######## COUNT VISITS OVER LIFETIME
+	Lifetime=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT COUNT(*) from Master WHERE CardNumber = '$CardNumber' 
+								AND Vm_VisitsAccrued = '1'")
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+	######## 
+	Lifetimereal="$(($MinBal+$Lifetime))"
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+	######## MINIMUM VISITBALNCE
+	VmVB=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT MAX(Vm_Visitsbalance) from Master WHERE CardNumber = '$CardNumber'")
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
 
-echo 'DEV.PX.CA.PROCESS.SH COMPLETED'
+	##### GET MAX  TRANSACTIONDATE
+	MaxDate=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT MAX(TransactionDate) from Master WHERE CardNumber = '$CardNumber'")
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR	
+
+	##### GET 2ND TO MAX TRANSACTIONDATE
+	SecondMax=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT TransactionDate from Master WHERE CardNumber = '$CardNumber' 
+										AND Vm_VisitsAccrued = '1' ORDER BY TransactionDate DESC limit 1,1") 
+	trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+	##### IF SECONDMAX IS NULL / EMPTY
+	if [ -z $SecondMax ]
+	then
+
+		##### UPDATE ONLY FIRST FREQUENCIES
+		mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET FreqCurrent = DATEDIFF(NOW(), '$MaxDate'), Freq12mos = '$PrevYear', 
+									FreqLifetime = '$Lifetimereal'  WHERE CardNumber = '$CardNumber'"
+		trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR	
+		#echo $CardNumber" first only MAX "$MaxDate" 2ND "$SecondMax"  Prevyr "$PrevYear" VmVB "$VmVB 
+		#echo "PrevLifereal "$Lifetimereal" prevlifenotreal"$Lifetime" MinBal "$MinBal
+
+		##### IF SECONDMAX HAS A VALUE
+		else
+			##### GET 3RD TO MAX TRANSACTIONDATE
+			ThirdMax=$(mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "SELECT transactiondate from Master WHERE CardNumber = '$CardNumber' 
+										AND Vm_VisitsAccrued = '1' ORDER BY TransactionDate DESC limit 2,1") 
+			trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+			##### IF THIRDMAX IS NULL / EMPTY
+			if [ -z $ThirdMax ]
+			then
+				##### UPDATE ONLY FIRST AND SECOND FREQUENCIES
+				mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET FreqCurrent = DATEDIFF(NOW(), '$MaxDate'), 
+										FreqRecent = DATEDIFF('$MaxDate', '$SecondMax'), Freq12mos = '$PrevYear', 
+										FreqLifetime = '$Lifetimereal'  WHERE CardNumber = '$CardNumber'"
+				trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR			   
+				#echo $CardNumber" first and second MAX "$MaxDate" 2ND "$SecondMax" Prevyr "$PrevYear" VmVB "$VmVB
+				#echo " PrevLifereal "$Lifetimereal" prevlifenotreal "$Lifetime" MinBal "$MinBal
+				
+			##### IF THIRDMAX HAS A VALUE
+			else
+				##### UPDATE ALL FREQUENCIES
+				mysql  --login-path=local -DSRG_Prod-1-27-20 -N -e "UPDATE Master SET FreqCurrent = DATEDIFF(NOW(), '$MaxDate'), 
+										FreqRecent = DATEDIFF('$MaxDate', '$SecondMax'), 
+										FreqPrevious = DATEDIFF('$SecondMax', '$ThirdMax'), Freq12mos = '$PrevYear', 
+										FreqLifetime = '$Lifetimereal'   WHERE CardNumber = '$CardNumber'"
+			trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+			#echo $CardNumber" first second third MAX "$MaxDate" 2ND "$SecondMax" 3RD "$ThirdMax" Prevyr "$PrevYear" VmVB "$VmVB
+			#echo " PrevLifereal "$Lifetimereal" prevlifenotreal "$Lifetime" MinBal "$MinBal
+
+				
+			fi
+
+		fi
+done || trap 'failfunction ${?} ${LINENO} "$BASH_COMMAND"' ERR
+echo 'MASTER TABLE FREQUENCY FIELDS UPDATED ****NO**** VM Visit fix applied'
 
 
 
